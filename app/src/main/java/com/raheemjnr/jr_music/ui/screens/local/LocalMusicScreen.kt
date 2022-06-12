57a734bce6fc69bab1961ad7920174f8d846c91a
Jnr <mumedian6@gmail.com>
2022-06-12 14:36:23 +0100

    added splash screen

--- app/src/main/java/com/raheemjnr/jr_music/ui/screens/local/LocalMusicScreen.kt
+++ app/src/main/java/com/raheemjnr/jr_music/ui/screens/local/LocalMusicScreen.kt
@@ -21,12 +21,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.ActivityCompat.startIntentSenderForResult
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.raheemjnr.jr_music.ui.viewmodels.MainViewModel
-import com.raheemjnr.jr_music.utils.Permissions
 import com.raheemjnr.jr_music.utils.askPermissions
 import com.raheemjnr.jr_music.utils.hasPermissions
 
@@ -45,38 +42,26 @@ fun LocalMusicScreen() {
 
     val audios = viewModel.audios.observeAsState()
 
-    val intentSender = viewModel.permissionNeededForDelete.observeAsState()
+//    val intentSender = viewModel.permissionNeededForDelete.observeAsState()
+//
+//    // On Android 10+, if the app doesn't have permission to modify
+//    // or delete an item, it returns an `IntentSender` that we can
+//    // use here to prompt the user to grant permission to delete (or modify)
+//    // the image.
+//    intentSender.value?.let { intentSender ->
+//        startIntentSenderForResult(
+//            context,
+//            intentSender,
+//            DELETE_PERMISSION_REQUEST,
+//            null,
+//            0,
+//            0,
+//            0,
+//            null
+//        )
+//    }
 
-    // On Android 10+, if the app doesn't have permission to modify
-    // or delete an item, it returns an `IntentSender` that we can
-    // use here to prompt the user to grant permission to delete (or modify)
-    // the image.
-    intentSender.value?.let { intentSender ->
-        startIntentSenderForResult(
-            context,
-            intentSender,
-            DELETE_PERMISSION_REQUEST,
-            null,
-            0,
-            0,
-            0,
-            null
-        )
-    }
 
-    val multiplePermissionsState = rememberMultiplePermissionsState(
-        listOf(
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        )
-    )
-    Permissions(
-        multiplePermissionsState = multiplePermissionsState,
-        context = context,
-        rationaleText = "But for this app to serve you properly we need this permission. Please, grant us access on the Settings screen.",
-        modifier = Modifier,
-        viewModel = viewModel
-    )
     Column(
         Modifier.fillMaxSize()
     ) {
@@ -91,28 +76,32 @@ fun LocalMusicScreen() {
                     context, 100, Manifest.permission.READ_EXTERNAL_STORAGE,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE
                 )
+
             }
+
         }) {
             Text(text = "show audios")
         }
         //
         LazyVerticalGrid(cells = GridCells.Adaptive(128.dp)) {
-            items(audios.value!!.size) {
-                Card(
-                    backgroundColor = Color.Red,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .fillMaxWidth(),
-                    elevation = 8.dp,
-                ) {
-                    Text(
-                        text = audios.value.toString(),
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 30.sp,
-                        color = Color(0xFFFFFFFF),
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.padding(16.dp)
-                    )
+            audios.value?.let {
+                items(it.size) {
+                    Card(
+                        backgroundColor = Color.Red,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth(),
+                        elevation = 8.dp,
+                    ) {
+                        Text(
+                            text = audios.value.toString(),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 30.sp,
+                            color = Color(0xFFFFFFFF),
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
                 }
             }
 
---
+++ app/src/main/java/com/raheemjnr/jr_music/utils/extraz.kt
@@ -0,0 +1,24 @@
+package com.raheemjnr.jr_music.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+
+//check if permission is already granted
+fun hasPermissions(context: Context, vararg permissions: String) =
+    permissions.all { permission ->
+        ActivityCompat.checkSelfPermission(
+            context,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+//else ask permission
+fun askPermissions(context: Context, requestCode: Int, vararg permissions: String) {
+    ActivityCompat.requestPermissions(
+        context as Activity,
+        permissions,
+        requestCode
+    )
+}
