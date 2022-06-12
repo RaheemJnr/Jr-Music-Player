57449cd1a90d2dea1eeacfec1b2eb349cd76ce49
Jnr <mumedian6@gmail.com>
2022-06-12 13:10:38 +0100

    added splash screen

---
+++ app/src/main/java/com/raheemjnr/jr_music/data/model/MediaAudio.kt
@@ -0,0 +1,13 @@
+package com.raheemjnr.jr_music.data.model
+
+import android.net.Uri
+
+/**
+ * Simple data class to hold information about audio included in the device's MediaStore.
+ */
+data class MediaAudio(
+    val id: Long,
+    val displayName: String,
+    val duration: Int,
+    val contentUri: Uri
+)
