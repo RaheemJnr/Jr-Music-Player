e928b4fd96e0f5d89d245ec3189174403810def9
Jnr <mumedian6@gmail.com>
2022-06-20 15:15:05 +0100

    logged all audio files to log cat

--- app/src/main/java/com/raheemjnr/jr_music/navigation/destinations.kt
+++ app/src/main/java/com/raheemjnr/jr_music/navigation/destinations.kt
@@ -4,21 +4,8 @@ import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.ui.graphics.vector.ImageVector
 
 
-//sealed class AuthScreen(val route: String) {
-//    object Login : AuthScreen("login")
-//    object ForgetPassword : AuthScreen("forgetPassword")
-//    object AuthType : AuthScreen("authType")
-//    object UserType : AuthScreen("userType")
-//    object UserInfo : AuthScreen("userInfo")
-//}
-
 sealed class MainScreen(val route: String?, val title: String?, val icon: ImageVector?) {
     object Local : MainScreen("Local", "Local", Icons.Default.Add)
     object Gap : MainScreen(null, null, null)
     object Online : MainScreen("Online", "Online", Icons.Default.Notifications)
-//    object Profile : MainScreen("profile", "Profile", Icons.Default.Person)
-//    object Camera : MainScreen("camera", null, null)
-//    object Admin : MainScreen("admin", null, null)
-//    object Contractor : MainScreen("contractor", null, null)
-
 }
