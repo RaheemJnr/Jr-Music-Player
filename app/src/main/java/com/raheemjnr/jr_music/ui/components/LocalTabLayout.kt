16f32fd4ba145f03f1daa576155977534236d61e
Jnr <mumedian6@gmail.com>
2022-08-14 21:00:00 +0100

    tab layout fixes

--- app/src/main/java/com/raheemjnr/jr_music/ui/components/LocalTabLayout.kt
+++ app/src/main/java/com/raheemjnr/jr_music/ui/components/LocalTabLayout.kt
@@ -49,7 +49,7 @@ fun LocalTabLayout(
                 selected = pagerState.currentPage == index,
                 onClick = {
                     scope.launch {
-                        pagerState.animateScrollToPage(page = index, pageOffset = 0.2f)
+                        pagerState.animateScrollToPage(page = index, pageOffset = 1f)
                     }
                 },
             )
