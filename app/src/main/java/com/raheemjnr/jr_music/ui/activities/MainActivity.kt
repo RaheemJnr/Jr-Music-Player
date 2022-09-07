b0f6567a6ef7d097360868837960088b49bf904b
Jnr <mumedian6@gmail.com>
2022-09-07 11:10:37 +0100

    commit test

--- app/src/main/java/com/raheemjnr/jr_music/ui/activities/MainActivity.kt
+++ app/src/main/java/com/raheemjnr/jr_music/ui/activities/MainActivity.kt
@@ -12,8 +12,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,6 +24,7 @@ import androidx.navigation.compose.rememberNavController
 import com.raheemjnr.jr_music.navigation.MainScreenNavigation
 import com.raheemjnr.jr_music.ui.components.BottomNav
 import com.raheemjnr.jr_music.ui.components.BottomTrackController
+import com.raheemjnr.jr_music.ui.components.NowPlaying
 import com.raheemjnr.jr_music.ui.theme.JrMusicPlayerTheme
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
@@ -51,40 +53,57 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
-    Scaffold(
-        bottomBar = {
-            Column {
-                BottomTrackController(
-                    trackName = "HH",
-                    seekState = 0.5F,
-                    imageUrl = "",
-                    nowPlayingClicked = { /*TODO*/ },
-                    artistName = "Jnr",
-                    isPlaying = true,
-                    onChangePlayerClicked = { /*TODO*/ },
-                    hasLiked = true
-                ) {
-
-                }
-                BottomAppBar(
-                    modifier = Modifier
-                        .height(65.dp)
-                        .clip(RoundedCornerShape(0.dp, 0.dp, 0.dp, 0.dp)),
-                    cutoutShape = CircleShape,
-                    backgroundColor = MaterialTheme.colors.primary,
-                    elevation = 22.dp,
-                ) {
-                    BottomNav(
-                        navController = navController,
-                    )
-                }
-            }
-        },
-        floatingActionButtonPosition = FabPosition.Center,
-        isFloatingActionButtonDocked = true,
-    ) {
-        MainScreenNavigation(navController)
+    val isPlayerOpening = remember {
+        mutableStateOf(true)
     }
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
+    )
+    BottomSheetScaffold(
+        sheetContent = {
+            NowPlaying(
+                isPlayer = isPlayerOpening.value,
+            )
+        },
+        sheetPeekHeight = 0.dp,
+        scaffoldState = bottomSheetScaffoldState,
+    ) {
+        Scaffold(
+            bottomBar = {
+                Column {
+                    BottomTrackController(
+                        trackName = "HH",
+                        seekState = 0.5F,
+                        imageUrl = "",
+                        nowPlayingClicked = { isPlayerOpening.value = true },
+                        artistName = "Jnr",
+                        isPlaying = true,
+                        onChangePlayerClicked = { /*TODO*/ },
+                        hasLiked = true
+                    ) {
+
+                    }
+                    BottomAppBar(
+                        modifier = Modifier
+                            .height(65.dp)
+                            .clip(RoundedCornerShape(0.dp, 0.dp, 0.dp, 0.dp)),
+                        cutoutShape = CircleShape,
+                        backgroundColor = MaterialTheme.colors.primary,
+                        elevation = 22.dp,
+                    ) {
+                        BottomNav(
+                            navController = navController,
+                        )
+                    }
+                }
+            },
+            floatingActionButtonPosition = FabPosition.Center,
+            isFloatingActionButtonDocked = true,
+        ) {
+            MainScreenNavigation(navController)
+        }
+    }
+
 }
 
 
