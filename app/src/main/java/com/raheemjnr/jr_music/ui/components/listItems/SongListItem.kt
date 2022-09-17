9b20b88c5d27c036908c3c5bcc31da0e7f283eea
Jnr <mumedian6@gmail.com>
2022-09-17 15:14:26 +0100

    fixing  UI icons

--- app/src/main/java/com/raheemjnr/jr_music/ui/components/listItems/SongListItem.kt
+++ app/src/main/java/com/raheemjnr/jr_music/ui/components/listItems/SongListItem.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
@@ -135,7 +134,7 @@ fun SongListItem(
                 painter = painterResource(id = R.drawable.morevert),
                 contentDescription = "",
                 modifier = Modifier
-                    .clickable {
+                    .noRippleClickable {
                         showToast(
                             context = context,
                             message = "clicked moreVert", Toast.LENGTH_SHORT
