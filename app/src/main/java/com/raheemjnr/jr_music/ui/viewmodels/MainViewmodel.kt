dfb25af436e0bb6a982c71bacaa1c23b9b06aca2
Raheem Jnr <mumedian6@gmail.com>
2022-10-14 13:29:23 +0100

    fixing bugs

--- app/src/main/java/com/raheemjnr/jr_music/ui/viewmodels/MainViewmodel.kt
+++ app/src/main/java/com/raheemjnr/jr_music/ui/viewmodels/MainViewmodel.kt
@@ -1,24 +1,27 @@
 package com.raheemjnr.jr_music.ui.viewmodels
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.database.ContentObserver
 import android.provider.MediaStore
+import android.util.Log
 import androidx.lifecycle.*
 import com.raheemjnr.jr_music.data.model.Songs
 import com.raheemjnr.jr_music.media.MusicServiceConnection
+import com.raheemjnr.jr_music.media.extentions.id
+import com.raheemjnr.jr_music.media.extentions.isPlayEnabled
+import com.raheemjnr.jr_music.media.extentions.isPlaying
+import com.raheemjnr.jr_music.media.extentions.isPrepared
+import com.raheemjnr.jr_music.utils.TAG
 import com.raheemjnr.jr_music.utils.loadMusic
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
-import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     @ApplicationContext private val applicationContext: Context,
-    musicServiceConnection: MusicServiceConnection
+   private val musicServiceConnection: MusicServiceConnection
 
 ) : ViewModel() {
     private val _audio = MutableLiveData<List<Songs>>()
@@ -39,90 +42,64 @@ class MainViewModel @Inject constructor(
 
 
     /**
-     * Convenience method to convert a day/month/year date into a UNIX timestamp.
+     * * This method takes a [Songs] and does one of the following:
+     * - If the item is *not* the active item, then play it directly.
+     * - If the item *is* the active item, check whether "pause" is a permitted command. If it is,
+     *   then pause playback, otherwise send "play" to resume playback.
      *
-     * We're suppressing the lint warning because we're not actually using the date formatter
-     * to format the date to display, just to specify a format to use to parse it, and so the
-     * locale warning doesn't apply.
      */
-    @Suppress("SameParameterValue")
-    @SuppressLint("SimpleDateFormat")
-    private fun dateToTimestamp(day: Int, month: Int, year: Int): Long =
-        SimpleDateFormat("dd.MM.yyyy").let { formatter ->
-            TimeUnit.MICROSECONDS.toSeconds(formatter.parse("$day.$month.$year")?.time ?: 0)
+
+
+    fun playMedia(mediaItem: Songs, pauseAllowed: Boolean = true) {
+        val nowPlaying = musicServiceConnection.nowPlaying.value
+        val transportControls = musicServiceConnection.transportControls
+
+        val isPrepared = musicServiceConnection.playbackState.value?.isPrepared ?: false
+        if (isPrepared && mediaItem.id == nowPlaying?.id) {
+            musicServiceConnection.playbackState.value?.let { playbackState ->
+                when {
+                    playbackState.isPlaying ->
+                        if (pauseAllowed) transportControls.pause() else Unit
+                    playbackState.isPlayEnabled -> transportControls.play()
+                    else -> {
+                        Log.w(
+                            TAG, "Playable item clicked but neither play nor pause are enabled!" +
+                                    " (mediaId=${mediaItem.id})"
+                        )
+                    }
+                }
+            }
+        } else {
+            transportControls.playFromMediaId(mediaItem.id, null)
         }
+    }
+
+    fun playMediaId(mediaId: String) {
+        val nowPlaying = musicServiceConnection.nowPlaying.value
+        val transportControls = musicServiceConnection.transportControls
+
+        val isPrepared = musicServiceConnection.playbackState.value?.isPrepared ?: false
+        if (isPrepared && mediaId == nowPlaying?.id) {
+            musicServiceConnection.playbackState.value?.let { playbackState ->
+                when {
+                    playbackState.isPlaying -> transportControls.pause()
+                    playbackState.isPlayEnabled -> transportControls.play()
+                    else -> {
+                        Log.w(
+                            TAG, "Playable item clicked but neither play nor pause are enabled!" +
+                                    " (mediaId=$mediaId)"
+                        )
+                    }
+                }
+            }
+        } else {
+            transportControls.playFromMediaId(mediaId, null)
+        }
+    }
 
 
-/*
-private fun updateState(
-playbackState: PlaybackStateCompat,
-mediaMetadata: MediaMetadataCompat
-): List<Songs> {
 
-val newResId = when (playbackState.isPlaying) {
-true -> R.drawable.morevert
-else -> R.drawable.play_button
-}
 
-return _audio.value?.map {
-val useResId = if (it.id == mediaMetadata.id) newResId else NO_RES
-it.copy(id = useResId.toString())
-} ?: emptyList()
-}
-
-/ **
-* This method takes a [Songs] and does one of the following:
-* - If the item is *not* the active item, then play it directly.
-* - If the item *is* the active item, check whether "pause" is a permitted command. If it is,
-*   then pause playback, otherwise send "play" to resume playback.
-* /
-fun playMedia(mediaItem: Songs, pauseAllowed: Boolean = true) {
-val nowPlaying = musicServiceConnection.nowPlaying.value
-val transportControls = musicServiceConnection.transportControls
-
-val isPrepared = musicServiceConnection.playbackState.value?.isPrepared ?: false
-if (isPrepared && mediaItem.id == nowPlaying?.id) {
-musicServiceConnection.playbackState.value?.let { playbackState ->
-when {
-playbackState.isPlaying ->
-if (pauseAllowed) transportControls.pause() else Unit
-playbackState.isPlayEnabled -> transportControls.play()
-else -> {
-Log.w(
-TAG, "Playable item clicked but neither play nor pause are enabled!" +
-" (mediaId=${mediaItem.id})"
-)
-}
-}
-}
-} else {
-transportControls.playFromMediaId(mediaItem.id, null)
-}
-}
-
-fun playMediaId(mediaId: String) {
-val nowPlaying = musicServiceConnection.nowPlaying.value
-val transportControls = musicServiceConnection.transportControls
-
-val isPrepared = musicServiceConnection.playbackState.value?.isPrepared ?: false
-if (isPrepared && mediaId == nowPlaying?.id) {
-musicServiceConnection.playbackState.value?.let { playbackState ->
-when {
-playbackState.isPlaying -> transportControls.pause()
-playbackState.isPlayEnabled -> transportControls.play()
-else -> {
-Log.w(
-TAG, "Playable item clicked but neither play nor pause are enabled!" +
-" (mediaId=$mediaId)"
-)
-}
-}
-}
-} else {
-transportControls.playFromMediaId(mediaId, null)
-}
-}
-*/
 
     /**
      * Since we register a [ContentObserver], we want to unregister this when the `ViewModel`
--- app/src/main/java/com/raheemjnr/jr_music/ui/viewmodels/SongListScreenViewModel.kt
+++ app/src/main/java/com/raheemjnr/jr_music/ui/viewmodels/SongListScreenViewModel.kt
@@ -1,16 +1,25 @@
 package com.raheemjnr.jr_music.ui.viewmodels
 
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import com.raheemjnr.jr_music.R
 import com.raheemjnr.jr_music.data.model.Songs
+import com.raheemjnr.jr_music.media.EMPTY_PLAYBACK_STATE
 import com.raheemjnr.jr_music.media.MusicServiceConnection
+import com.raheemjnr.jr_music.media.NOTHING_PLAYING
+import com.raheemjnr.jr_music.media.extentions.id
+import com.raheemjnr.jr_music.media.extentions.isPlaying
+import com.raheemjnr.jr_music.utils.Constants.NO_RES
 import com.raheemjnr.jr_music.utils.Constants.mediaId
+import javax.inject.Inject
 
-class SongListScreenViewModel(
+class SongListScreenViewModel @Inject constructor(
+    musicServiceConnection: MusicServiceConnection
 ) : ViewModel() {
     /**
      * Use a backing property so consumers of mediaItems only get a [LiveData] instance so
@@ -20,56 +29,43 @@ class SongListScreenViewModel(
     val audios: LiveData<List<Songs>> get() = _audio
 
 
-
     private val subscriptionCallback = object : MediaBrowserCompat.SubscriptionCallback() {
         override fun onChildrenLoaded(
             parentId: String,
             children: List<MediaBrowserCompat.MediaItem>
         ) {
-//            val itemsList = children.map { child ->
-//                val subtitle = child.description.subtitle ?: ""
-//                MediaItemData(
-//                    child.mediaId!!,
-//                    child.description.title.toString(),
-//                    subtitle.toString(),
-//                    child.description.iconUri!!,
-//                    child.isBrowsable,
-//                    getResourceForMediaId(child.mediaId!!)
-//                )
-//            }
-//            _mediaItems.postValue(itemsList)
         }
     }
+
 
     /**
     //     * When the session's [PlaybackStateCompat] changes, the [mediaItems] need to be updated
     //     * so the correct [MediaItemData.playbackRes] is displayed on the active item.
     //     * (i.e.: play/pause button or blank)
     //     */
-/*
-private val playbackStateObserver = Observer<PlaybackStateCompat> {
-val playbackState = it ?: EMPTY_PLAYBACK_STATE
-val metadata = musicServiceConnection.nowPlaying.value ?: NOTHING_PLAYING
-if (metadata.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID) != null) {
-_audio.postValue(updateState(playbackState, metadata))
-}
-}
 
-/ **
-* When the session's [MediaMetadataCompat] changes, the [mediaItems] need to be updated
-* as it means the currently active item has changed. As a result, the new, and potentially
-* old item (if there was one), both need to have their [MediaItemData.playbackRes]
-* changed. (i.e.: play/pause button or blank)
-* /
-private val mediaMetadataObserver = Observer<MediaMetadataCompat> {
-val playbackState = musicServiceConnection.playbackState.value ?: EMPTY_PLAYBACK_STATE
-val metadata = it ?: NOTHING_PLAYING
-if (metadata.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID) != null) {
-_audio.postValue(updateState(playbackState, metadata))
-}
-}
-*/
 
+    private val playbackStateObserver = Observer<PlaybackStateCompat> {
+        val playbackState = it ?: EMPTY_PLAYBACK_STATE
+        val metadata = musicServiceConnection.nowPlaying.value ?: NOTHING_PLAYING
+        if (metadata.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID) != null) {
+            _audio.postValue(updateState(playbackState, metadata))
+        }
+    }
+
+    /**
+     * When the session's [MediaMetadataCompat] changes, the [mediaItems] need to be updated
+     * as it means the currently active item has changed. As a result, the new, and potentially
+     * old item (if there was one), both need to have their [MediaItemData.playbackRes]
+     * changed. (i.e.: play/pause button or blank)
+     */
+    private val mediaMetadataObserver = Observer<MediaMetadataCompat> {
+        val playbackState = musicServiceConnection.playbackState.value ?: EMPTY_PLAYBACK_STATE
+        val metadata = it ?: NOTHING_PLAYING
+        if (metadata.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID) != null) {
+            _audio.postValue(updateState(playbackState, metadata))
+        }
+    }
 
     /**
      * Because there's a complex dance between this [ViewModel] and the [MusicServiceConnection]
@@ -89,11 +85,37 @@ _audio.postValue(updateState(playbackState, metadata))
      * which can also change the [MediaItemData.playbackRes]s in the list.
      */
 
-//    private val musicServiceConnection = musicServiceConnection.also {
-//        it.subscribe(mediaId, subscriptionCallback)
-//
-//        it.playbackState.observeForever(playbackStateObserver)
-//        it.nowPlaying.observeForever(mediaMetadataObserver)
-//    }
+    private val musicServiceConnection = musicServiceConnection.also {
+        it.subscribe(mediaId, subscriptionCallback)
+
+        it.playbackState.observeForever(playbackStateObserver)
+        it.nowPlaying.observeForever(mediaMetadataObserver)
+    }
+
+    private fun updateState(
+        playbackState: PlaybackStateCompat,
+        mediaMetadata: MediaMetadataCompat
+    ): List<Songs> {
+
+        val newResId = when (playbackState.isPlaying) {
+            true -> R.drawable.morevert
+            else -> R.drawable.play_button
+        }
+
+        return _audio.value?.map {
+            val useResId = if (it.id == mediaMetadata.id) newResId else NO_RES
+            it.copy(id = useResId.toString())
+        } ?: emptyList()
+    }
+
+
+    override fun onCleared() {
+        // Remove the permanent observers from the MusicServiceConnection.
+        musicServiceConnection.playbackState.removeObserver(playbackStateObserver)
+        musicServiceConnection.nowPlaying.removeObserver(mediaMetadataObserver)
+
+        // And then, finally, unsubscribe the media ID that was being watched.
+        musicServiceConnection.unsubscribe(mediaId, subscriptionCallback)
+    }
 
 }
